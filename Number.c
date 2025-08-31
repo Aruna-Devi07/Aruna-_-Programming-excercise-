c7f4d4f7348995a0aab225264c6ebba42a396abf
Aruna Devi <arunadevissunil13@gmail.com>
2025-08-31 11:58:42 +0530

    Create Number.c

---
+++ Number.c
@@ -0,0 +1,13 @@
+// To find sign of number
+#include <stdio.h>
+
+int main(){
+    int n;
+
+    printf("Enter Number: ");
+    scanf("%d",&n);
+
+    if (n > 0)
+    {
+        printf("Number Is Positive");
+    }
