238d5f06a8664388dd66ae04a41afc8ad2994263
Aruna Devi <arunadevissunil13@gmail.com>
2025-08-31 21:08:58 +0530

    Create Area_circle.cpp

---
+++ Area _ circle.cpp/Area_circle.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int r;
+	int area;
+
+	printf("Enter Radius: ");
+	scanf("%d",&r);
+
+	area=3.14*r*r;
+
+	printf("The Area Is %d", area);
+	printf("cm^2");
+	return 0;
+}
