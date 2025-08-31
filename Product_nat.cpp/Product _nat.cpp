819ad62da0d1b8bb70eeee30cfef340ee05695e6
Aruna Devi <arunadevissunil13@gmail.com>
2025-08-31 21:30:16 +0530

    Create Product _nat.cpp

---
+++ Product_nat.cpp/Product _nat.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+int main()
+{
+ int n;
+ long prod=1;
+ int i=1;
+
+ std::cout << "Enter Number: ";
+ std::cin >> n;
+
+ if (n<=0)
+ {
+ std::cout << "Enter A Natural Number";
+ return 1;
+ }
+
+ while (i<=n)
+ {
+ prod*=i;
+ i++;
+ }
+
+ std::cout << "The Product Of The First " << n << " Natural Numbers is " << prod << std::endl;
+}
