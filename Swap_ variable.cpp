4c1b56dc5b5d2779382a032837f8c0c691cac31a
Aruna Devi <arunadevissunil13@gmail.com>
2025-08-31 13:10:41 +0530

    Create Swap_ variable.cpp

---
+++ Swap_ variable.cpp
@@ -0,0 +1,15 @@
+//Swapping Variables
+#include <stdio.h>
+
+int main(){
+    int a;
+    int b;
+    int temp;
+
+    printf("Enter First Variable: ");
+    scanf("%d",&a);
+
+    printf("Enter Second Variable: ");
+    scanf("%d",&b);
+
+    printf("Before Swapping: a=%
