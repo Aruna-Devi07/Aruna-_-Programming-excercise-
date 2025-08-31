58332dee10086ef0e857c1f4b1e641496b7f67eb
Aruna Devi <arunadevissunil13@gmail.com>
2025-08-31 12:11:42 +0530

    Create Remainder.cpp

---
+++ Remainder.cpp
@@ -0,0 +1,13 @@
+//Checks Remainder
+#include <stdio.h>
+
+int main(){
+    int n1;
+    int n2;
+    int remainder;
+
+    printf("Enter Dividend: ");
+    scanf("%d",&n1);
+
+    printf("Enter Divisor: ");
+    scanf("%d",&
