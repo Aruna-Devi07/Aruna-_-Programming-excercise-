dbb474b8f88c65c119d8f370ec075e31e9860067
Aruna Devi <arunadevissunil13@gmail.com>
2025-08-31 21:28:39 +0530

    Create Sum_nat.cpp

---
+++ Sum_nat.cpp/Sum_nat.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+ int n;
+ int sum=0;
+ int i=1;
+
+ printf("Enter Number N: ");
+ scanf("%d",&n);
+
+ while (i<=n)
+ {
+ sum+=n;
+ i++;
+ }
+
+ printf("The Sum Of First %d Numbers is %d", n , sum);
+}
