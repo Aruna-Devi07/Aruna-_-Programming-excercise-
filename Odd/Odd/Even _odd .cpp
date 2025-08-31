bb677c3f848f46f53c3a3fbe8a0d5ea8118c3faa
Aruna Devi <arunadevissunil13@gmail.com>
2025-08-31 13:17:56 +0530

    Create Even _odd .cpp

---
+++ Odd/Odd/Even _odd .cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+
+    printf("Enter Number: ");
+    scanf("%d",&n);
+
+    if (n%2==0)
+    {
+        printf("The Number Is Even");
+    }
+    else
+    {
+        printf("The Number Is Odd");
+    }
+
+    return 0;
+}
