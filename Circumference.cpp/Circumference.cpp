f844a3aa08042bc458fccf81614829368d6b85ba
Aruna Devi <arunadevissunil13@gmail.com>
2025-08-31 21:15:24 +0530

    Create Circumference.cpp

---
+++ Circumference.cpp/Circumference.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int r;
+    int Circumference;
+
+    printf("Enter Radius: ");
+    scanf("%d",&r);
+
+    Circumference=2*3.14*r;
+
+    printf("The Circumference Is %d", Circumference);
+    printf("cm^2");
+
+    return 0;
+}
