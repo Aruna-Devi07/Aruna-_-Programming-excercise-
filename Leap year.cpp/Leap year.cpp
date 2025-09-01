b4efbd22621f16d56b582d9514b4d60a12f755e4
Aruna Devi <arunadevissunil13@gmail.com>
2025-09-01 22:20:58 +0530

    Create Leap year.cpp

---
+++ Leap year.cpp/Leap year.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+int main(){
+    int year;
+
+    std::cout << "Enter Year: ";
+    std::cin >> year;
+
+    if (year%400==0)
+    {
+        std::cout << "Is A Leap Year" << std::endl;
+    }
+
+    else if (year%4==0)
+    {
+        std::cout << "Is A Leap Year" << std::endl;
+    }
+
+    else if (year%100==0)
+    {
+        std::cout << "Is Not A Leap Year" << std::endl;
+    }
+
+    else
+    {
+        std::cout << "Is Not A Lea" << std::endl;
+    }
+}
